bf0165f0ac358c2b1ad61f87ed5d12588564b9f2
Bryan Bosely <bbosely@us.ibm.com>
2025-04-29 11:41:06 -0400

    chore(SCC) Manual change integration test listProfiles()
    Signed-off-by: Bryan Bosely <bbosely@us.ibm.com>

--- test/integration/security-and-compliance-center-api.v3.test.js
+++ test/integration/security-and-compliance-center-api.v3.test.js
@@ -639,7 +639,6 @@ describe('SecurityAndComplianceCenterApiV3_integration', () => {
       instanceId: 'acd7032c-15a3-484f-bf5b-67d41534d940',
       accountId: accountIdForReportLink,
       limit: 50,
-      start: 'testString',
     };
 
     const res = await securityAndComplianceCenterApiService.listProfiles(params);
