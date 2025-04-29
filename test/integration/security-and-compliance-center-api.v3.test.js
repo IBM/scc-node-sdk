f54d1461a00c872d88c642da03a68ac4a90ef7af
Bryan Bosely <bbosely@us.ibm.com>
2025-04-29 12:34:11 -0400

    chore(SCC) Manual change integration test listReportEvaluations() via ReportEvaluationsPager
    Signed-off-by: Bryan Bosely <bbosely@us.ibm.com>

--- test/integration/security-and-compliance-center-api.v3.test.js
+++ test/integration/security-and-compliance-center-api.v3.test.js
@@ -1329,17 +1329,9 @@ describe('SecurityAndComplianceCenterApiV3_integration', () => {
     const params = {
       instanceId: 'acd7032c-15a3-484f-bf5b-67d41534d940',
       reportId: reportIdForReportLink,
-      assessmentId: 'testString',
-      assessmentMethod: 'testString',
-      componentId: 'testString',
-      targetId: 'testString',
-      targetEnv: 'testString',
-      targetName: 'testString',
       status: 'failure',
       limit: 10,
       sort: 'assessment_id',
-      scopeId: 'testString',
-      subscopeId: 'testString',
     };
 
     const allResults = [];
